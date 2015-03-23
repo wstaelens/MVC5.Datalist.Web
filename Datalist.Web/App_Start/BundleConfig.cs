6b8b568d6b168efc65b63ea2c5fc95314d4f676e
Muchiachio <aldis.rameika@gmail.com>
2015-03-23 10:38:22 +0200

    Content bundling
    - Added support for bundling web site content.

---
+++ Datalist.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Optimization;
+
+namespace Datalist.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            RegisterScripts(bundles);
+            RegisterStyles(bundles);
+        }
+        private static void RegisterScripts(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/JQueryUI/Bundle").Include("~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Datalist/Bundle").Include("~/Scripts/Datalist/datalist.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/JQuery/Bundle").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/QUnit/Bundle").Include("~/Scripts/qunit.js"));
+        }
+        private static void RegisterStyles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/JQueryUI/Theme/Bundle").Include("~/Content/themes/base/all.css"));
+            bundles.Add(new StyleBundle("~/Content/Bootstrap/Bundle").Include("~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/Datalist/Bundle").Include("~/Content/Datalist/*.css"));
+            bundles.Add(new StyleBundle("~/Content/QUnit/Bundle").Include("~/Content/qunit.css"));
+            bundles.Add(new StyleBundle("~/Content/Shared/Bundle").Include("~/Content/site.css"));
+        }
+    }
+}
--- Datalist.Web/Global.asax.cs
+++ Datalist.Web/Global.asax.cs
@@ -1,5 +1,5 @@
 ﻿using System.Web;
-using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Datalist.Web
@@ -8,8 +8,8 @@ namespace Datalist.Web
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
