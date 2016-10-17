755cde2ebcfab4637a5e7362224ea8756e7ad210
Muchiachio <aldis.rameika@gmail.com>
2016-10-17 10:25:05 +0300

    Content bundling
    - Moved most of the content files to single file bundles, to decrease
    content requests on page load.

--- Datalist.Web/App_Start/BundleConfig.cs
+++ Datalist.Web/App_Start/BundleConfig.cs
@@ -11,18 +11,20 @@ namespace Datalist.Web
         }
         private static void RegisterScripts(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/Scripts/MvcDatalist/Bundle").Include("~/Scripts/MvcDatalist/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/JQueryUI/Bundle").Include("~/Scripts/JQueryUI/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/JQuery/Bundle").Include("~/Scripts/JQuery/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/Shared/Bundle").Include("~/Scripts/Shared/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Page/Bundle")
+                .Include("~/Scripts/JQuery/*.js")
+                .Include("~/Scripts/JQueryUI/*.js")
+                .Include("~/Scripts/MvcDatalist/*.js")
+                .Include("~/Scripts/Shared/*.js"));
         }
         private static void RegisterStyles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/MvcDatalist/Bundle").Include("~/Content/MvcDatalist/*.css"));
-            bundles.Add(new StyleBundle("~/Content/FontAwesome/Bundle").Include("~/Content/FontAwesome/*.css"));
-            bundles.Add(new StyleBundle("~/Content/Bootstrap/Bundle").Include("~/Content/Bootstrap/*.css"));
-            bundles.Add(new StyleBundle("~/Content/JQueryUI/Bundle").Include("~/Content/JQueryUI/*.css"));
-            bundles.Add(new StyleBundle("~/Content/Shared/Bundle").Include("~/Content/Shared/*.css"));
+            bundles.Add(new StyleBundle("~/Content/Page/Bundle")
+                .Include("~/Content/JQueryUI/*.css")
+                .Include("~/Content/Bootstrap/*.css")
+                .Include("~/Content/FontAwesome/*.css")
+                .Include("~/Content/MvcDatalist/*.css")
+                .Include("~/Content/Shared/*.css"));
         }
     }
 }
